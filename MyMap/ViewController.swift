48e48c88a3b9cd2444ec316c4eadc0fac06032c9
ryo.takahashi <ryo.takahashi@ryotakahashinoMacBook-Pro.local>
2017-12-27 20:41:34 +0900

    first commit

--- MyMap/ViewController.swift
+++ MyMap/ViewController.swift
@@ -7,17 +7,62 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController ,UITextFieldDelegate{
+    
+    @IBOutlet weak var inputText: UITextField!
+    
+    @IBOutlet weak var displayMap: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        inputText.delegate = self
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // キーボードを引っ込める
+        textField.resignFirstResponder()
+        
+        // テキストフィールドに入力された値を取り出す
+        let searchKeyword = textField.text!
+        
+        print(searchKeyword)
+        
+        let geocoder = CLGeocoder()
+        
+        geocoder.geocodeAddressString(searchKeyword, completionHandler: {
+            (placemarks: [CLPlacemark]?,error:Error?) in
+            // placemarksに検索結果リストが出る
+            // 0番目が最も近い場所の位置情報
+            if let placemark = placemarks?[0] {
+                if let targetCoordinate = placemark.location?.coordinate {
+                    // 位置情報データ
+                    print(targetCoordinate)
+                    
+                    let pin = MKPointAnnotation()
+                    pin.coordinate = targetCoordinate
+                    pin.title = searchKeyword
+                    
+                    // ピンのアノテーション
+                    self.displayMap.addAnnotation(pin)
+                    //　表示する範囲
+                    self.displayMap.region =
+                        MKCoordinateRegionMakeWithDistance(targetCoordinate, 500.0, 500.0)
+                    
+                    
+                }
+            }
+        })
+        
+        return true
     }
 
 
